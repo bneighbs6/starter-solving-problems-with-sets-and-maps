4c4a0a50e81dd57b83955249815416dcb399873c
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-22 10:53:09 -0600

    Solved sumPairs. 10/15 passing

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -2,7 +2,38 @@
  * Return an array of pairs of numbers that sum to N
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
+ * 
+ * initialize a new Map named numbers
+ * initialize a new Map named solution
+ * 
+ * for each element e in A do:
+ * add e to numbers
+ * 
+ * for each element e in A do;
+ * calculate diff = N - e
+ * if diff is not equal to e then  (1)
+ * look up diff in numbers
+ * if diff is in numbers then
+ * add [e, diff] to solution (2)
+ * 
+ * convert solution to an array and return it
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+
+    for (let elem of A) {
+        numbers.set(elem, undefined);
+    }
+
+    for (let elem of A) {
+        let diff = N - elem;
+        if (diff !== elem && numbers.has(diff)) {
+            solution.set([elem, diff], undefined);
+        }
+    }
+
+    return [...solution.keys()];
+}
 
 module.exports = sumPairs;
